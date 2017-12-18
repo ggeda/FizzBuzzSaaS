7aa312cc5cbd4148d698908afe9a0aaca9f0cbfd
Genene Geda <genene.geda@pb.com>
2017-12-18 09:56:01 -0700

    typo list bizz buzz

--- src/main/java/com/example/fizzbuzzsaas/fuzzbuzz/FBServiceImpl.java
+++ src/main/java/com/example/fizzbuzzsaas/fuzzbuzz/FBServiceImpl.java
@@ -20,18 +20,18 @@ public class FBServiceImpl implements FBService {
 		Map<String, List<Integer>> fbMapList = new HashMap<>();
 		List<Integer> fizzList = new ArrayList<>();
 		List<Integer> buzzList = new ArrayList<>();
-		List<Integer> fizzBizzList = new ArrayList<>();
+		List<Integer> fizzBuzzList = new ArrayList<>();
 
 		IntStream.rangeClosed(1, upperlimit).parallel().forEachOrdered(i -> {
 
 			if (isFizz(i)) fizzList.add(i);
 			if (isBuzz(i)) buzzList.add(i);
-			if (isFizzBuzz(i)) fizzBizzList.add(i);
+			if (isFizzBuzz(i)) fizzBuzzList.add(i);
 		});
 
 		fbMapList.put("Fizz", fizzList);
 		fbMapList.put("Buzz", buzzList);
-		fbMapList.put("FizzBuzz", fizzBizzList);
+		fbMapList.put("FizzBuzz", fizzBuzzList);
 		return fbMapList;
 	}
 
